c149bc348e68751d5bfcace897d7e5022e3b34ed
sunjikang <537217771@qq.com>
2021-04-21 11:22:50 +0800

    log

--- app/src/main/java/com/zhong/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/zhong/kotlindemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zhong.kotlindemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     //sjk branch
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView
         setContentView(R.layout.activity_main)
+        val ss = "sjk"
+        Log.e("ss",ss)
     }
 }
